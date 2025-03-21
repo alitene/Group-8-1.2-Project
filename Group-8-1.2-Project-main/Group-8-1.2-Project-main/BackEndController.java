15b9bcbe9e5b782c1feb4b1a5035caa2f9a71b55
Polluxe <Polluxe16@outlook.com>
2025-03-21 16:36:44 -0400

    Update BackEndController.java

--- Group-8-1.2-Project-main/Group-8-1.2-Project-main/BackEndController.java
+++ Group-8-1.2-Project-main/Group-8-1.2-Project-main/BackEndController.java
@@ -1,4 +1,6 @@
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
